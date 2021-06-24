e7206569dd059c0ec5059bae40bc0488b756fd70
Aleksey Egorov <aleksey358358@gmail.com>
2021-06-25 00:05:06 +0300

    Update file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
@@ -13,37 +15,35 @@ contacts_list1 = []
 pattern = r"(8|\+7)\s*\(?(\d{3})[-\)\s]?\s*(\d{3})-?(\d{2})-?(\d+)\s?[\s\(]?\w*\.*\s*(\d*)\)*"
 sub1 = r"+7(\2)\3-\4-\5"
 sub2 = r"+7(\2)\3-\4-\5 доб.\6"
-for cl in contacts_list:
-    cl1 = []
-    for c in cl:
-        result = re.match(pattern, c)
+for contact_list in contacts_list:
+    contact_list1 = []
+    for contact in contact_list:
+        result = re.match(pattern, contact)
         if result is not None:
             if result.group(6) != "":
-                result1 = re.sub(pattern, sub2, c)
-                c = c.replace(c, result1)
+                result1 = re.sub(pattern, sub2, contact)
+                contact = contact.replace(contact, result1)
             else:
-                result2 = re.sub(pattern, sub1, c)
-                c = c.replace(c, result2)
+                result2 = re.sub(pattern, sub1, contact)
+                contact = contact.replace(contact, result2)
         else:
             pass
-        cl1.append(c)
-    contacts_list1.append(cl1)
+        contact_list1.append(contact)
+    contacts_list1.append(contact_list1)
 
-# pprint(contacts_list1)
+#pprint(contacts_list1)
 # Форматируем ФИО в соответствии с заданием
 contacts_list2 = []
-for cl in contacts_list1:
-    s = cl[3:7]
-    f = cl[0:3]
-    fr = [','.join(f)]
-    cl1 = []
-    for fra in fr:
-        fra = fra.replace(',', ' ').split()
-        if len(fra) < 3:
-            fra.append('')
-        #print(fra)
-        cl1 = fra + s
-    contacts_list2.append(cl1)
+for contact_list in contacts_list1:
+    second_part = contact_list[3:7]
+    first_part = contact_list[0:3]
+    first_part = [','.join(first_part)]
+    for first_part_str in first_part:
+        first_part_list = first_part_str.replace(',', ' ').split()
+        if len(first_part_list) < 3:
+            first_part_list.append('')
+        contact_list1 = first_part_list + second_part
+    contacts_list2.append(contact_list1)
 #pprint(contacts_list2)
 
 
